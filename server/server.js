1e9f7009ac8faf01670a8c7c208f4dcc673eb395
Vivek S. Pal <165067090+VvkCoder@users.noreply.github.com>
2026-05-11 23:54:28 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,9 +18,7 @@ app.use(express.json())
 
 // Email transporter
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
