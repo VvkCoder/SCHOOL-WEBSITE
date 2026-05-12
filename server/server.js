339bf07e52b04ab8f3f590b7e441db9ae2cbc22f
Vivek S. Pal <165067090+VvkCoder@users.noreply.github.com>
2026-05-13 01:15:24 +0530

    Change SMTP port for nodemailer
    Updated SMTP port from 587 to 2525 in nodemailer configuration.

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.json())
 // Email transporter
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
+  port: 2525,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
