1a55f42efc900ed229acacaa5f2429dad1454b02
Vivek S. Pal <165067090+VvkCoder@users.noreply.github.com>
2026-05-13 00:31:41 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,12 +14,14 @@ app.use(express.json())
 
 // Email transporter
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
-});
+})
 
 transporter.verify((error, success) => {
   if (error) {
